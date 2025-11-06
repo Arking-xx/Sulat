fdb7cfcc97b1b9509ed5527a0860b1535b91d763
Arking-xx <harkdevx@gmail.com>
2025-11-07 00:33:20 +0800

    remove submithandler

--- app/src/features/auth/SignIn.tsx
+++ app/src/features/auth/SignIn.tsx
@@ -1,7 +1,7 @@
 import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useForm, type SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { signInSchema, type userLogin } from '../../validation/formSchema';
 import { useAuth } from '../../hooks/auth/useAuth';
