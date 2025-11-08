266ad10edd1f665cd3f4c85a7ce47cc25be617af
Arking-xx <harkdevx@gmail.com>
2025-11-08 22:29:25 +0800

    feat: reusable button

---
+++ app/src/features/components/ui/Button.tsx
@@ -0,0 +1,11 @@
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+const Button = ({ children, className, ...rest }: ButtonProps) => {
+  return <button className={className} {...rest}>{children}</button>;
+};
+
+export default Button
