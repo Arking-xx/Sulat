892b1bd7c220824648a17ff5c1165efd95c15023
Arking-xx <harkdevx@gmail.com>
2025-11-08 22:30:01 +0800

    feat: reusable x button

---
+++ app/src/features/components/ui/Xbutton.tsx
@@ -0,0 +1,11 @@
+import { Cross1Icon } from '@radix-ui/react-icons';
+
+const Xbutton = () => {
+  return (
+    <a href="/">
+      <Cross1Icon className="" />
+    </a>
+  );
+};
+
+export default Xbutton;
