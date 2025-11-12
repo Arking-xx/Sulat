8ec9779fbf0f830b7457ba14ed87b132375a8c7d
Arking-xx <harkdevx@gmail.com>
2025-11-12 14:22:00 +0800

    fix: dynamic route

--- app/src/hooks/useHideLayout.tsx
+++ app/src/hooks/useHideLayout.tsx
@@ -1,19 +1,32 @@
 import { useLocation, matchPath } from 'react-router-dom';
+
 export function useHideLayout() {
   const location = useLocation();
 
-  const validRoutes =
-    ['/', '/signup', '/signin', '/profile', '/write', '/posts', '/profile/updateProfile'].includes(
-      location.pathname
-    ) ||
-    matchPath('/post/:slug', location.pathname) ||
-    matchPath('/profile/user/:id', location.pathname) ||
-    matchPath('/post/update/:slug', location.pathname);
+  const routes = [
+    '/',
+    '/signup',
+    '/signin',
+    '/profile',
+    '/write',
+    '/posts',
+    '/profile/updateProfile',
+  ];
+
+  const path = ['/post/:slug', '/profile/user/:id', '/post/update/:slug'];
+  const excludeFirstElement = routes.slice(1);
+  const data = path.some((el) => matchPath(`${el}`, location.pathname));
+  const validRoutes = routes.includes(location.pathname) || data;
+  console.log('exclude', excludeFirstElement);
 
   return {
+    hideLayout: excludeFirstElement.includes(location.pathname) || data,
+
     hideSidebar: ['/', '/signin', '/signup'].includes(location.pathname) || !validRoutes,
 
     hideInHomePage: ['/'].includes(location.pathname),
+
+    redirect: routes.includes(location.pathname) || data,
 
     hideElements:
       ['/write'].includes(location.pathname) ||
