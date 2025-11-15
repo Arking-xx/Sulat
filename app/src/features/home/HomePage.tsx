852e4e8704a0bda16bcddbd70c5e503c3f8fabf5
Arking-xx <harkdevx@gmail.com>
2025-11-15 16:02:04 +0800

    fix: styling, feat: about

--- app/src/features/home/HomePage.tsx
+++ app/src/features/home/HomePage.tsx
@@ -10,12 +10,12 @@ export default function HomePage() {
       {!hideLayout && (
         <Navbar
           className={
-            'absolute bg-primary shadow-lg flex items-center justify-between py-3 md:px-18 top-0 left-0 w-full'
+            'sticky z-10 bg-primary shadow-lg flex items-center justify-between py-3 md:px-18 top-0 left-0 w-full'
           }
         />
       )}
-      <div className=" h-screen   flex sm:justify-center md:justify-start items-center overflow-hidden">
-        <div className="flex flex-col gap-5 md:px-24  ">
+      <div className=" h-screen flex sm:justify-center md:justify-start mt-30 ">
+        <div className="flex flex-col gap-5 sm:px-5 md:px-24  w-screen overflow-hidden">
           <div>
             <h1 className="font-lora sm:text-5xl md:text-8xl text-text-color font-medium ">
               Filipino
@@ -25,7 +25,7 @@ export default function HomePage() {
             </h2>
             <div className="py-3 sm:w-80 md:w-lg">
               <h3 className="font-roboto sm:text-2xl  md:text-3xl text-text-color font-light">
-                A place to share and enjoy Filipino stories and culture.
+                A place to share and enjoy Filipino stories.
               </h3>
             </div>
           </div>
@@ -40,6 +40,23 @@ export default function HomePage() {
             />
           </div>
         </div>
+      </div>
+
+      <div id="about" className="flex items-center sm:mx-10 md:mx-24 h-50 sm:mt-10 md:mt-0">
+        <section className="">
+          <div className="grid grid-cols-2 gap-10">
+            <h2 className="font-lora sm:text-xl md:text-5xl sm:bg-gray-400 sm:text-white md:text-black md:bg-white h-fit">
+              Every Filipino has a story waiting to be heard.
+            </h2>
+            <p className="font-lora text-justify sm:text-sm md:text-xl mt-20 sm:mb-20 md:mb-50">
+              Sulat is a website where every Filipino can share their daily stories and ideas.
+              Anyone can share their knowledge with people on the internet. This platform isn’t just
+              about learning — it’s also a place to meet new people. My ultimate goal in creating
+              Sulat is to help Filipinos connect with others and discover different cultures across
+              the Philippines.
+            </p>
+          </div>
+        </section>
       </div>
       <Footer />
     </div>
